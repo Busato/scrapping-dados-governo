f576415b080ce9d476abeea96408917eb6b46600
Nicolas Busato <nicolas.busato@getmore.com.br>
2019-09-25 12:22:30 -0300

    ta andando

--- crawlers/crawlerUol.js
+++ crawlers/crawlerUol.js
@@ -1,55 +1,96 @@
-const URL = 'https://noticias.uol.com.br/'
+// https://github.com/GoogleChromeLabs/puppeteer-examples/blob/master/crawlsite.js
 
-module.exports = {
-    crawlUOL: puppeteer => {
-        puppeteer
-            .launch({ headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] })
-            .then(async browser => {
+const crawledPages = new Map()
+const arrayOfNews = []
 
-            const page = await browser.newPage()
+const Crawl = module.exports = {
+    crawlUOL: async (browser, page) => {
+  
+        // If we've already crawled the URL, we know its children.
+        if (crawledPages.has(page.url)) {
+          console.log(`Reusing route: ${page.url}`);
+    
+          const item = crawledPages.get(page.url);
+          console.log(item)
+          page.title = item.title;
+          page.img = item.img;
+          page.children = item.children;
+          // Fill in the children with details (if they already exist).
+          page.children.forEach(c => {
+            const item = crawledPages.get(c.url);
+            c.title = item ? item.title : '';
+            c.img = item ? item.img : null;
+          });
+          return;
+        } else {
+          console.log(`Loading: ${page.url}`);
+      
+          // Setting new father page
+          const newPage = await browser.newPage();
+          await newPage.goto(page.url, {waitUntil: 'networkidle2'});
+          await newPage.setViewport({width: 320, height: 600})
+          await newPage.addScriptTag({url: 'https://code.jquery.com/jquery-3.2.1.min.js'})
+
+          // Get all the children pages and filter them
+          let anchors = await newPage.evaluate(collectAllSameOriginAnchorsDeep);
+          anchors = anchors.filter(a => a !== page.url) // link doesn't point to start url of crawl.
             
-            // Initiate default browser
-            await page.setViewport({width: 320, height: 600})
-            await page.setUserAgent('Mozilla/5.0 (iPhone; CPU iPhone OS 9_0_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13A404 Safari/601.1')
-            await page.goto(URL, {waitUntil: 'networkidle0'})
-            await page.addScriptTag({url: 'https://code.jquery.com/jquery-3.2.1.min.js'})
+          console.log(anchors)
+          page.title = await newPage.evaluate('document.title');
+          page.children = anchors.map(url => ({url}));
+      
+          crawledPages.set(page.url, page); // cache it.
+      
+          await newPage.close();
+        }
+        
+        // Crawl subpages.
+        for (const childPage of page.children) {
+          await Crawl.crawlUOL(browser, childPage);
+        }
 
-            const result = await page.evaluate(() => {
-                try {
-                    var data = []
-                    const urlArray = []
-
-                    $('.highlights-headline').each(function() {
-                        urlArray.push($(this).find('a').attr('href'))
-                    })
-
-
-
-                    // for (let currentUrl of urlArray) {
-                    //     await page.goto(currentUrl)
-                    //     await page.waitForNavigation({ waitUntil: 'networkidle' })
-                    //     let currentTitle = $(this).find('title').attr('text')
-
-                        // data.push({
-                        //     'title' : currentTitle
-                        // })
-
-                    // }
-                    
-                    return urlArray // Return our data array
-                } catch(err) {
-                    reject(err.toString())
-                }
-            })
-            
-            await browser.close()
-
-            console.log(result)
-
-            process.exit()
-        }).catch(function(error) {
-            console.error('No way Paco!')
-            process.exit()
-        })
-    }
+      }
 }
+
+
+   collectAllSubPages = () => {
+    const allElements = [];
+  
+    $('.highlights-headline .row').each(function() {
+        let currentLink = $(this).find('a').attr('href')
+
+        allElements.push(currentLink)
+    })
+    
+    return Array.from(new Set(allElements));
+  }
+
+
+  function collectAllSameOriginAnchorsDeep(sameOrigin = true) {
+    const allElements = [];
+  
+    const findAllElements = function(nodes) {
+      for (let i = 0, el; el = nodes[i]; ++i) {
+        allElements.push(el);
+        // If the element has a shadow root, dig deeper.
+        if (el.shadowRoot) {
+          findAllElements(el.shadowRoot.querySelectorAll('*'));
+        }
+      }
+    };
+  
+    findAllElements(document.querySelectorAll('*'));
+  
+    const filtered = allElements
+      .filter(el => el.localName === 'a' && el.href) // element is an anchor with an href.
+      .filter(el => el.href !== location.href) // link doesn't point to page's own URL.
+      .filter(el => {
+        if (sameOrigin) {
+          return new URL(location).origin === new URL(el.href).origin;
+        }
+        return true;
+      })
+      .map(a => a.href);
+  
+    return Array.from(new Set(filtered));
+  }
