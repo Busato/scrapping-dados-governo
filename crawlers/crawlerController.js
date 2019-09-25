5b2af5ffbf0faec1d8bc4b3a22627649c781f4e5
Nicolas Busato <nicolas.busato@getmore.com.br>
2019-09-25 15:45:15 -0300

    ta voando

--- crawlers/crawlerController.js
+++ crawlers/crawlerController.js
@@ -1,5 +1,6 @@
 const puppeteer = require('puppeteer')
 const uolCrawler = require("./crawlerUol")
+const fs = require('fs')
 
 module.exports = {
     initiateCrawling: async () => {
@@ -8,8 +9,24 @@ module.exports = {
         const browser = await puppeteer.launch();
             
         // Initiate specific crawlers
-        await uolCrawler.crawlUOL(browser, {url: `https://noticias.uol.com.br/`})
+        let arrayOfNewsUol = await uolCrawler.crawlUOL(browser, {url: `https://noticias.uol.com.br/`})
+
+        const arrayOfNews = {
+            table: []
+        }
+
+        arrayOfNews.table.push(arrayOfNewsUol)
+
+        appendNewsToJson(arrayOfNews)
 
         await browser.close()
     }
+}
+
+appendNewsToJson = arrayOfNews => {
+    fs.writeFile('news.json', JSON.stringify(arrayOfNews, null, 2), (err) => {
+        if (err) console.error(err)
+        console.log('Data written to file')
+    })
+
 }
